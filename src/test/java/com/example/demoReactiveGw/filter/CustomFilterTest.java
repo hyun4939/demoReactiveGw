d0bb47028ef7893755ac095150e02c10db5f6e8c
jack <hyun4939@naver.com>
2024-06-11 16:25:50 +0900

    불필요코드 삭제

--- src/test/java/com/example/demoReactiveGw/filter/CustomFilterTest.java
+++ src/test/java/com/example/demoReactiveGw/filter/CustomFilterTest.java
@@ -1,36 +1,20 @@
 package com.example.demoReactiveGw.filter;
 
 import com.example.demoReactiveGw.filter.config.FilterConfig;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
 import lombok.extern.slf4j.Slf4j;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-import org.mockito.Mockito.*;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.http.server.reactive.MockServerHttpResponse;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
-import java.util.Iterator;
-
-//import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
-import static reactor.core.publisher.Mono.when;
-import static org.junit.jupiter.api.Assumptions.*;
 @Slf4j
 @ExtendWith(SpringExtension.class)
 public class CustomFilterTest {
